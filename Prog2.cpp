d30d7b14f42d3b60dbfda84b0b40bb31b71ba0b8
AdityaMathur017 <aditya25csu017@ncuindia.edu>
2026-01-21 16:45:26 +0530

    adding question

---
+++ Prog2.cpp
@@ -0,0 +1,12 @@
+//AREA OF A CIRCLE
+#include<iostream>
+int main()
+{
+    float radius;
+    float area;
+    std::cout<<"enter the radius";
+    std::cin>>radius;
+    area=3.14*radius*radius;
+    std::cout<<"the area of the circle is: "<<area;
+    return 0;
+}
